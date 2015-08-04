4594a300155ab3c35e72786b194d9fdc33fd8363
SuminP <ghjk3554@naver.com>
2015-08-04 17:11:34 +0900

    Update ABCheck.rb

--- def-ABCheck-str-ab-str.scan/a...b/ABCheck.rb
+++ def-ABCheck-str-ab-str.scan/a...b/ABCheck.rb
@@ -1,12 +1,1 @@
-def ABCheck(str)
 
-ab = str.scan(/a...b/)
-return ab.any?
-# code goes here
-
-      
-end
-  
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-ABCheck(STDIN.gets)           
